ddeed02bc10ce01f656f626c54c34c7a99ed8415
Mike Lee <leemike0429@gmail.com>
2021-01-26 11:57:00 +0800

    新增action lunch

---
+++ WebApplication1/Controllers/testController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult lunch()
+        {
+            return View();
+        }
+    }
+}
